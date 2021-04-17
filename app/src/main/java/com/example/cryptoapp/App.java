fc82dd9e1ccfb1426f952eba590db8f4b79718ec
dinkiagrawal-15 <dinkiagrawal77@gmail.com>
2021-04-17 20:12:03 +0000

    initial commit

---
+++ app/src/main/java/com/example/cryptoapp/App.java
@@ -0,0 +1,44 @@
+package com.example.cryptoapp;
+
+import android.app.AlertDialog;
+import android.app.Application;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        Configuration newConfig = new Configuration();
+        newConfig.locale = Locale.ENGLISH;
+        super.onConfigurationChanged(newConfig);
+        Locale.setDefault(newConfig.locale);
+        getBaseContext().getResources().updateConfiguration(newConfig,getResources().getDisplayMetrics());
+    }
+    public static void Loger(String m){
+        Log.e("mip tag",m);
+    }
+    public static void ToastMaker(Context c, String m){
+        Toast.makeText(c,m,Toast.LENGTH_SHORT).show();
+    }
+
+    public static void DailogMaker(Context c , String title , String mess){
+        AlertDialog.Builder alert = new AlertDialog.Builder(c);
+        alert.setTitle(title);
+        alert.setMessage(mess);
+        alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        alert.show();
+    }
+}
